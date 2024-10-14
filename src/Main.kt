5ce9ae8ca00c30f1b7f015380834f1a1456f5b29
Luzeq123 <morkovkin465pro@gmail.com>
2024-10-14 18:28:39 +0700

    add function sayAge

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,14 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    sayName( "Ярослав")
+    sayAge(  13)
+}
 
+fun sayName(name : String){
+    println("Привет,$name")
+}
+
+fun sayAge(age : Int){
+    println("Тебе, $age лет")
 }
